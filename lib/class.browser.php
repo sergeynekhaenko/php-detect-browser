a69c806811bf93dabbf677b47cba4a55ceead9e7
Sergey Nehaenko <admin@n-people.net>
2012-04-02 16:18:17 +0300

    v 1.0.1

--- lib/class.browser.php
+++ lib/class.browser.php
@@ -524,23 +524,4 @@ class browser
 		}
 	}
 }
-$browser = new browser();
-echo '<br>';
-foreach($browser->browser as $key => $value)
-{
-echo $key.': '.$value.'<br>';	
-}
-echo '[Browser]<br>';
-foreach($browser->browser['browser'] as $key => $value)
-{
-echo $key.': '.$value.'<br>';	
-}
-if(isset($browser->browser['os']))
-{
-echo '[OS]<br>';
-foreach($browser->browser['os'] as $key => $value)
-{
-echo $key.': '.$value.'<br>';	
-}
-}
 ?>
